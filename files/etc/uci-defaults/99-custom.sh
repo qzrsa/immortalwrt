5b69e0ce3a0653078c7b02d740c035037f67e7f6
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2025-03-25 17:23:36 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -101,7 +101,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by 丶曲終人散ゞ"
+NEW_DESCRIPTION="by 丶曲終人散ゞ"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
